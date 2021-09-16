a2590b4bc59d0aaca97aeea5e829b91792c1d1ca
André dos Santos <andsantos@pagseguro.com>
2021-09-16 18:30:02 -0300

    change script and gradle

--- src/gatling/scala/ragnarok/Ragnarok.scala
+++ src/gatling/scala/ragnarok/Ragnarok.scala
@@ -3,10 +3,12 @@ package ragnarok
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
+import scala.concurrent.duration.DurationInt
+
 class Ragnarok extends Simulation {
 
   val httpProtocol = http
-    .baseUrl("https://tranquil-temple-33251.herokuapp.com/accounts") // Here is the root for all relative URLs
+    .baseUrl("http://sheltered-shelf-55259.herokuapp.com/reactive/accounts") // Here is the root for all relative URLs
 
   val scn = scenario("Chamar partner-account-api")
     .exec(http("request")
@@ -14,5 +16,12 @@ class Ragnarok extends Simulation {
       .check(status.is(200))
     )
 
-  setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
+//  setUp(scn.inject(atOnceUsers(17)).protocols(httpProtocol))
+
+  setUp(
+    scn.inject(
+      atOnceUsers(10), // 2
+      constantUsersPerSec(300).during(1.minute), // 4
+    ).protocols(httpProtocol)
+  )
 }
