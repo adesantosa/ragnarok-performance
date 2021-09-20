2c92865030f6f2ad594eb89e95b8ba19d8a8ad4e
Douglas Henrique <dohenrique@uolinc.com>
2021-09-20 18:35:52 -0300

    Added post scenario

---
+++ README.md
@@ -0,0 +1,6 @@
+# Rodar o gatling
+./gradlew gatlingRun
+
+# Limite encontrado no account-reactive-api
+constantUsersPerSec(35).during(1.minute) // 1800
+
--- src/gatling/scala/ragnarok/Ragnarok.scala
+++ src/gatling/scala/ragnarok/Ragnarok.scala
@@ -10,18 +10,33 @@ class Ragnarok extends Simulation {
   val httpProtocol = http
     .baseUrl("http://sheltered-shelf-55259.herokuapp.com/reactive/accounts") // Here is the root for all relative URLs
 
-  val scn = scenario("Chamar partner-account-api")
+  val scn = scenario("List accounts")
     .exec(http("request")
       .get("/")
       .check(status.is(200))
     )
 
+  val scn2 = scenario("Create account")
+    .exec(http("request")
+      .post("/")
+      .body(RawFileBody("clientId.json"))
+      .asJson
+      .check(status.is(200))
+    )
+
 //  setUp(scn.inject(atOnceUsers(17)).protocols(httpProtocol))
 
+//  setUp(
+//    scn.inject(
+//      atOnceUsers(10), // 2
+//      constantUsersPerSec(300).during(1.minute), // 4
+//    ).protocols(httpProtocol)
+//  )
+
   setUp(
-    scn.inject(
-      atOnceUsers(10), // 2
-      constantUsersPerSec(300).during(1.minute), // 4
+    scn2.inject(
+//      atOnceUsers(1),
+      constantUsersPerSec(30).during(1.minute), // 4
     ).protocols(httpProtocol)
   )
 }
